64e98d1d2fbb44e46e49c6b20b41f89f0501152e
Jess Woods <45497968+jkwoods@users.noreply.github.com>
2019-11-07 11:15:30 -0500

    Update set_up.py

--- set_up.py
+++ set_up.py
@@ -19,15 +19,15 @@ with open('fold_parameters.json') as json_file: #left some currently unused rest
     data = json.load(json_file)
     name = data['name']
     input_seq = data['input_seq']
-    #distance_rst = data['distanceRstFile']
+    distance_rst = data['distanceRstFile']
     #distance_force = data['distanceForce']
-    #torsion_rst = data['angleRstFile']
+    torsion_rst = data['angleRstFile']
     #torsion_force = data['angleForce']
     #temp = data['temp']
     #annealing_runs = int(data['cycles'])
     forcefield = data["forcefield"] #i've copied in ff14SB to the repo, bc otherwise hard to find on cades vs personal computers. Its Amber's proclaimed "best for protiens" forcefeild
 
-#open and read FASTA
+#open and read Jinbo input
 with open(input_seq) as f:
     lines = f.readlines()
     seq = ""
@@ -35,18 +35,13 @@ with open(input_seq) as f:
         seq = seq + l
 
 seq = seq.replace("\n", "") #clean
-
 triseq = "{ "+ seq + "}"
-
 print("Sequence is: "+ str(triseq))
 
-#generate Amber Tools helper file - tleap will not run unless everything is copied into the forcefield file
+#generate Amber Tools helper file - tleap will not run unless commands are copied into the forcefield file
 subprocess.call('cp '+forcefield+' amberscript', shell=True)
-
 h = open("amberscript", "a")
-
 h.write("\n"+name+" = sequence "+triseq+"\nsaveoff "+name+" linear.lib\nsavepdb "+name+" linear.pdb\nsaveamberparm "+name+" prmtop rst7\nquit")
-
 h.close()
 
 #call Amber Tools tleap
