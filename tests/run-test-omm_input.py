ed6ae9914c4f29977a86f8acf474cb9c01f2f03b
jrossyra <jrossyra@gmail.com>
2019-11-08 17:03:35 -0500

    test simple read/write/load system and read restraints success

--- tests/run-test-omm_input.py
+++ tests/run-test-omm_input.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python
 
-from OpenMMPlayground import (
+from xlafold import (
     OmmSystem, read_restraints,
 )
 from test_configuration import (
-    input_prefix, system_file
+    input_prefix, system_file, restraint_prefix
 )
 
+#---------- READ OpenMM Base System ------------------------#
 prmtop = input_prefix / "prmtop"
 inpcrd = input_prefix / "rst7"
 
 ommsystem = OmmSystem(
     ff_type="amber",
     topology=prmtop,
-    coordinates=inpcrd
+    coordinates=inpcrd,
 )
 
-ommsystem.save_xml(system_xml)
+ommsystem.save_xml(system_file)
+othsystem = OmmSystem(system_file=system_file)
 
-
-file_distance_restraints = input_prefix / "contacts.txt"
+#---------- READ Restraints --------------------------------#
+file_torsion_restraints  = restraint_prefix / "dihedral.tbl"
+file_distance_restraints = restraint_prefix / "contact.tbl"
+#torsion_restraints = read_restraints(file_torsion_restraints, "torsion")
 distance_restraints = read_restraints(file_distance_restraints, "distance")
 
-#system = system_from_xml(system_file)
-
+#---------- APPLY Restraints ------------------------------#
 for restraint_func, restraints in distance_restraints.items():
     print("Adding restraints with function: %s" % restraint_func)
     for rr, k in zip(*restraints):
--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -15,6 +15,7 @@ cwd           = Path.cwd()   ## runtime    path
 input_prefix  = cwd / "T0968s2_example_output"
 output_prefix = cwd / "omm_systems"
 system_file   = output_prefix / ("system-%s.xml" % system_name)
+restraint_prefix = cwd / "T0968s2_example_input"
 
 if not output_prefix.is_dir():
     output_prefix.mkdir()
