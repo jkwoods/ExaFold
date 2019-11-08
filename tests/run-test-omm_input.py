8e0954e80e02e106f61432973486daecba90c190
jrossyra <jrossyra@gmail.com>
2019-11-08 15:54:09 -0500

    test code in progress for omm setup and restraint read

--- tests/run-test-omm_input.py
+++ tests/run-test-omm_input.py
@@ -1,5 +1,30 @@
 #!/usr/bin/env python
 
-from test_configuration import *
+from OpenMMPlayground import (
+    OmmSystem, read_restraints,
+)
+from test_configuration import (
+    input_prefix, system_file
+)
+
+prmtop = input_prefix / "prmtop"
+inpcrd = input_prefix / "rst7"
+
+ommsystem = OmmSystem(
+    ff_type="amber",
+    topology=prmtop,
+    coordinates=inpcrd
+)
+
+ommsystem.save_xml(system_xml)
 
 
+file_distance_restraints = input_prefix / "contacts.txt"
+distance_restraints = read_restraints(file_distance_restraints, "distance")
+
+#system = system_from_xml(system_file)
+
+for restraint_func, restraints in distance_restraints.items():
+    print("Adding restraints with function: %s" % restraint_func)
+    for rr, k in zip(*restraints):
+        print(rr, k)
