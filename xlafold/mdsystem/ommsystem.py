9c5cfc1ce360825db92921a5cd4259ba8283ec59
jrossyra <jrossyra@gmail.com>
2019-11-12 10:46:15 -0500

    [mdsystem] topology added to get atom indices from structure

--- xlafold/mdsystem/ommsystem.py
+++ xlafold/mdsystem/ommsystem.py
@@ -2,9 +2,10 @@
 
 from time import sleep
 
-from simtk.openmm.app import AmberPrmtopFile, AmberInpcrdFile, NoCutoff
+from simtk.openmm.app import PDBFile, AmberPrmtopFile, AmberInpcrdFile, NoCutoff
 from simtk import openmm
 
+from mdtraj import Topology
 
 __all__ = ["OmmSystem"]
 
@@ -36,15 +37,27 @@ class OmmSystem(object):
         # parameters needed to generate all
         # the given restraints
         self._restraints = dict()
+        self._topology   = None
+
+        topofile  = kwargs.get("topology", None)
+        coordfile = kwargs.get("coordinates", None)
 
         if ff_type is not None:
             if ff_type.lower() == "amber":
-                prmtop = AmberPrmtopFile(kwargs["topology"])
-                inpcrd = AmberInpcrdFile(kwargs["coordinates"])
+                prmtop = AmberPrmtopFile(topofile)
+                inpcrd = AmberInpcrdFile(coordfile)
                 self.system = prmtop.createSystem(nonbondedMethod=NoCutoff)
+                self._topology = Topology.from_openmm(prmtop.topology)
 
         elif system_file is not None:
             self.load_xml(system_file)
+            if topofile:
+                if topofile.endswith(".pdb"):
+                    # this line is a bit silly but Topology class
+                    # doesn't seem to directly load PDB so keeps
+                    # the imports clean
+                    self._topology = Topology.from_openmm(
+                        PDBFile(topofile).topology)
 
         else:
             # Inspect and set ff_type
@@ -52,7 +65,20 @@ class OmmSystem(object):
             pass
 
 
-    def initialize_restraint_force(self, restraint_definition, atom_groups=list(), parameters=list()):
+    @property
+    def topology(self):
+        if not self._topology:
+            return None
+
+        elif hasattr(self._topology, "topology"):
+            return self._topology.topology
+
+        else:
+            return self._topology
+
+
+    def initialize_restraint_force(self, restraint_definition, interactions=list()):
+
         assert len(restraint_definition) == 1
         assert isinstance(restraint_definition, dict)
 
@@ -61,7 +87,19 @@ class OmmSystem(object):
         rd                = restraint_definition[restraint_type]
         restraint_formula = rd.get("formula", list())
         restraint_pars    = rd.get("parameters", list())
-        restraint_method  = rd.get("restraint", dict())
+
+        # Each interaction makes a restraint instance
+        # by calling instance_method w/ atom group
+        # the physical parameters
+        instance_call     = rd.get("restraint", dict())
+        instance_units    = rd.get("units", dict())
+        instance_method   = list(instance_call)[0]
+
+        assert len(instance_call) == 1
+        assert isinstance(instance_call[instance_method][0], int)
+
+        assert instance_call[instance_method][1] == len(instance_units)
+        instance_call[instance_method][1] = instance_units
 
         restraint_force   = getattr(openmm, restraint_type)(*restraint_formula)
     
@@ -71,24 +109,65 @@ class OmmSystem(object):
             getattr(restraint_force, method)(*parameter)
 
         self._restraints.update(
-            {restraint_type : [restraint_force, restraint_method]}
+            {restraint_type : [restraint_force, instance_call]}
         )
 
-        if atom_groups and parameters:
-            self.add_restraint_groups(restraint_type, atom_groups, parameters)
+        if interactions:
+            self.add_restraint_interactions(restraint_type, interactions)
 
 
-    def add_restraints(self, restraint_type, atom_groups, parameters):
+    def _aidx_from_resatom(self, resatom):
 
-        assert isinstance(atom_groups, (list, tuple))
-        assert isinstance(parameters, (list, tuple))
+        assert self.topology  # if None, can't get atom index from resatom
+
+        # MDTraj residues start numbering at 1
+        return int(self.topology.select("residue %d and name %s" % (
+            resatom[0], resatom[1].upper()
+        )))
+
+    def _format_interactions(self, restraint_type, interactions):
+
+        method       = list(self._restraints[restraint_type][1])[0]
+        method_pars  = self._restraints[restraint_type][1][method]
+        n_atoms      = method_pars[0]
+        par_units    = method_pars[1]
+
+        for interaction in interactions:
+            atom_indices = [
+                self._aidx_from_resatom(ra)
+                for ra in interaction[:n_atoms]
+            ]
+            parameters   = [
+                par*par_units[i]
+                for i,par in enumerate(interaction[n_atoms:])
+            ]
+
+            yield atom_indices, parameters
+
+
+    # Consider taking atom_group+parameters separately?
+    def add_restraint_interactions(self, restraint_type, interactions):
+
+        assert restraint_type in self._restraints
+        assert isinstance(interactions, (list, tuple))
 
         restraint_force, restraint_method = self._restraints[restraint_type]
+        interaction_method = list(restraint_method)[0]
 
-        for restraint in zip(atom_groups, parameters):
-            rargs = restraint[0]
-            rargs.append(restraint[1])
-            getattr(restraint_force, restraint_method)(*rargs)
+        for atom_group, parameters in self._format_interactions(
+            restraint_type, interactions
+        ):
+
+            assert isinstance(atom_group, list)
+
+            rargs = atom_group
+            rargs.append(parameters)
+
+            getattr(restraint_force, interaction_method)(*rargs)
+
+
+    def save_pdb(self, pdb_file):
+        PDBFile.write(pdb_file, self.topology)
 
 
     def save_xml(self, system_file):
