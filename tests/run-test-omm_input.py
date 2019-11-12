927be6599c6968d949ce747c748d89601c0b0c9e
jrossyra <jrossyra@gmail.com>
2019-11-12 10:47:51 -0500

    [tests] omm_input test creates all restraint instances after file read

---
+++ tests/run-test-omm_input.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+from xlafold import (
+    OmmSystem, read_restraints, OMM_RESTRAIN_distance
+)
+from test_configuration import (
+    input_prefix, system_file, restraint_prefix
+)
+
+from simtk import unit as u
+
+#---------- READ OpenMM Base System ------------------------#
+prmtop = input_prefix / "prmtop"
+inpcrd = input_prefix / "rst7"
+
+ommsystem = OmmSystem(
+    ff_type="amber",
+    topology=prmtop,
+    coordinates=inpcrd,
+)
+
+ommsystem.save_xml(system_file)
+othsystem = OmmSystem(system_file=system_file)
+
+#---------- READ Restraints --------------------------------#
+file_torsion_restraints  = restraint_prefix / "dihedral.tbl"
+file_distance_restraints = restraint_prefix / "contact.tbl"
+#torsion_restraints = read_restraints(file_torsion_restraints, "torsion")
+distance_restraints = read_restraints(file_distance_restraints, "distance")
+
+# Same force for each interaction right now
+f_r = 2.0
+[ri.append(f_r) for ri in distance_restraints]
+
+#---------- APPLY Restraints ------------------------------#
+#  - we select a restraint force implementation from
+#    list of those defined in the `xlafold` package
+restraint_name = "simpleharmonic_customforce"
+this_restraint = OMM_RESTRAIN_distance[restraint_name]
+restraint_type = list(this_restraint)[0]
+
+ommsystem.initialize_restraint_force(this_restraint)
+ommsystem.add_restraint_interactions(restraint_type, distance_restraints)
+
+
