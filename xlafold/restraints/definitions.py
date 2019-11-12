ccc7172a0d1fc413b2a58c069f1c62199866b389
jrossyra <jrossyra@gmail.com>
2019-11-12 10:47:13 -0500

    [restraints] definitions have reference name, encode parameter units

--- xlafold/restraints/definitions.py
+++ xlafold/restraints/definitions.py
@@ -5,6 +5,8 @@ __all__ = [
     "OMM_RESTRAIN_torsion",
 ]
 
+from simtk import unit as u
+
 
 # TODO expand to list of options
 #      for restraint implementation
@@ -16,24 +18,42 @@ OMM_RESTRAINT_types   = ["distance","torsion"]
 # FUNCTIONAL Encoding dicts
 #  -> the dict keys and vals are used
 #     programmatically, no typos!
+#  --> first one has distance-type restraints
+#  --> thus apply/require 2-particle input
 OMM_RESTRAIN_distance = dict(
-    # Name of OpenMM App Restraint Type
-    CustomBondForce=dict(
-        # given on outer App call
-        #  - can be empty
-        formula=["-k*(r-r0)^2"],
-        # one-time calls to Restraint
-        # type object
-        parameters=[
-            dict(addPerBondParameter=["k"]),
-            dict(addPerBondParameter=["r0"]),
-        ],
-        # Name and call signature for method
-        # to add each restraint instance..
-        # This case has:
-        # 1 restraint ~ [2 atoms, 2 parameters]
-        restraint=dict(addBond=[2,2])
+    # our reference name for a Restraint
+    # Force Group implementation
+    simpleharmonic_customforce=dict(
+        # Name of OpenMM App Restraint Type
+        #  --> must be length 1 dict with
+        #      name of OpenMM force class
+        CustomBondForce=dict(
+            # given on outer App call
+            #  - can be empty
+            formula=["-k*(r-r0)^2"],
+            # one-time calls to Restraint
+            # type object
+            parameters=[
+                dict(addPerBondParameter=["k"]),
+                dict(addPerBondParameter=["r0"]),
+            ],
+            # Name and call signature for method
+            # to add each restraint instance..
+            # This case has:
+            # 1 restraint ~ [2 atoms, 2 parameters]
+            restraint=dict(addBond=[2,2]),
+            # One for each parameter
+            # in order of parameters list
+            units=[
+                u.kilocalories_per_mole/u.angstrom**2,
+                u.angstrom
+            ],
+        ),
     ),
+    complexharmonic_customforce=dict(),
+    # TODO probably wrap the default implementations
+    #      and test if they run faster or not
+    harmonic_force=dict(),
 )
 
 OMM_RESTRAIN_torsion  = "CustomTorsionForce"
