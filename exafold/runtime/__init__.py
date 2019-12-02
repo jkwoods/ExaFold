684c91126070dbcd73be88c639f0b98526b1b277
jrossyra <jrossyra@gmail.com>
2019-12-02 13:42:56 -0500

    [runtime] module created with integration components

---
+++ exafold/runtime/__init__.py
@@ -0,0 +1,2 @@
+
+from .integrate import *
---
+++ exafold/runtime/integrate.py
@@ -0,0 +1,34 @@
+
+__all__ = [
+    "OMM_INTEGRATORS",
+    "OMM_BAROSTATS",
+    "OMM_THERMOSTATS",
+]
+
+from simtk import unit as u
+
+# OpenMM integrators with simple call signature
+# give argument names and corresponding simtk units
+# as fields of each integrator dict
+OMM_INTEGRATORS        = dict(
+    LangevinIntegrator = dict(
+        temperature    = u.kelvin,
+        frictionCoeff  = 1/u.picosecond,
+        stepSize       = u.femtosecond,
+    ),
+)
+
+OMM_THERMOSTATS            = dict(
+    AndersenThermostat     = dict(
+        defaultTemperature = u.kelvin,
+        defaultCollisionFrequency = 1/u.picosecond,
+    ),
+)
+
+OMM_BAROSTATS              = dict(
+    MonteCarloBarostat     = dict(
+        defaultPressure    = u.bar,
+        defaultTemperature = u.kelvin,
+        frequency          = 1, # this one in MD timesteps
+    ),
+)
