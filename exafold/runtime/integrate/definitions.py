754e2e54aaac3c2c5c9fd5f364153bf5b15715dd
John R Ossyra <jrossyra@gmail.com>
2019-12-06 11:42:35 -0500

    Update definitions.py

--- exafold/runtime/integrate/definitions.py
+++ exafold/runtime/integrate/definitions.py
@@ -7,18 +7,29 @@ from simtk import unit as u
 # as fields of each integrator dict
 class LangevinIntegrator(TimestepComponent):
 
+    # This list of 2-tuples defines the init signature
+    # for the underlying OMM object. We cannot give
+    # kwargs for some reason, they must be positional
+    # args so this list lets us easily bind arguments
+    # later via kwargs that will automatically be put
+    # in the correct order (see TimesteComponent.create
+    # method definition).
     kwargs = [
         ("temperature"  , u.kelvin),
         ("frictionCoeff", 1/u.picosecond),
         ("stepSize"     , u.femtosecond),
     ]
 
+    # Copy this and replace "LangevinIntegrator" with
+    # the name of whatever similar barostat/thermostat/
+    # integrator class from OMM to create addition
+    # exafold wrappers.
     def __init__(self):
         super(LangevinIntegrator, self).__init__(
             "LangevinIntegrator", LangevinIntegrator.kwargs)
 
 # TODO update these to use the TimestepComponent parent
-#      class following the LangevinIntegrator example
+#      class following the LangevinIntegrator example.
 AndersenThermostat     = dict(
     defaultTemperature = u.kelvin,
     defaultCollisionFrequency = 1/u.picosecond,
