8a47d30c7ade3dc1a00ae3b2a774e3a1dc2826ac
jrossyra <jrossyra@gmail.com>
2019-12-06 09:57:02 -0500

    walker test updates

--- tests/run-test-omm_walker.py
+++ tests/run-test-omm_walker.py
@@ -11,26 +11,22 @@ from simtk import unit as u
 #---------- READ OpenMM Base System ------------------------#
 prmtop = input_prefix / "prmtop"
 inpcrd = input_prefix / "rst7"
-
-ommsystem = OmmSystem(
-    ff_type="amber",
-    topology=prmtop,
-    coordinates=inpcrd,)
+ommsystem = OmmSystem(ff_type="amber", topology=prmtop, coordinates=inpcrd,)
 
 #---------- READ Restraints --------------------------------#
-distance_restraints = [
-    ri.append(2.0) for ri in
-    read_restraints(restraint_prefix / "contact.tbl", "distance")
-]
+restraints = read_restraints(restraint_prefix / "contact.tbl", "distance")
+[ri.append(2.0) for ri in restraints]
 
 #---------- APPLY Restraints ------------------------------#
 this_restraint = OMM_RESTRAIN_distance["simpleharmonic_customforce"]
 restraint_type = list(this_restraint)[0]
 ommsystem.initialize_restraint_force(this_restraint)
-ommsystem.add_restraint_interactions(restraint_type, distance_restraints[::10])
+ommsystem.add_restraint_interactions(restraint_type, restraints[::10])
 
 from exafold import Walker
+#from test_configuration import md_instructions
 
 walker = Walker()
-
-walker.create_simulation(ommsystem)
+#walker.set_instructions(md_instructions)
+walker.generate_simulation(ommsystem)
+walker.go()
